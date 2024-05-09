5a0fe5f93e66433e7a88ad91c2e6dfd9c9b78916
KILL3RHACKER <149474493+KILL3RHACKER@users.noreply.github.com>
2024-05-09 17:06:10 +0400

    Update casino.py

--- main/casino.py
+++ main/casino.py
@@ -2,17 +2,16 @@ import random
 import time
 
 ishod = ['МЕНЬШЕ', 'РАВНО', 'БОЛЬШЕ']
-random_ishod = random.choice(ishod)
 
 print("В данное приложение поможет вам обмануть казино. Подождите пока оно загрузится.")
 time.sleep(random.randint(5,10))
 userID = input("Введите ваш Telegram ID, чтобы его получить перейдите в бота: @getmyid_bot:  ")
-print("Ваш ID:", userID, "Все верно? Для ответа напишите ДА или НЕТ")
+print("Ваш ID:", userID)
 
-Type = input()
+print("Отлично. Выбирайте игру ЧИСЛА. Теперь пишите ИСХОД")
+chance = input()
+if chance == "ИСХОД":
+    random_ishod = random.choice(ishod)
+    print(random_ishod)
+    print("Введи СТОП или ИСХОД")
 
-if Type == "ДА":
-    print("Отлично. Выбирайте игру ЧИСЛА. Теперь пишите ИСХОД")
-    chance = input()
-    if chance == "ИСХОД":
-        print(random_ishod)
